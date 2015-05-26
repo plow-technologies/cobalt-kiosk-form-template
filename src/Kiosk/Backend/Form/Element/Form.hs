bc79173980aeea14185f0142e1e03e80cf0b7930
Scott Murphy <scottmurphy09@gmail.com>
2015-05-25 23:55:03 -0500

    not sure what chagned here

--- src/Kiosk/Backend/Form/Element/Form.hs
+++ src/Kiosk/Backend/Form/Element/Form.hs
@@ -10,34 +10,29 @@ Portability :  portable
 -}
 
 
-{-# LANGUAGE DeriveGeneric      #-}
-{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Kiosk.Backend.Form.Element.Form ( Form (..)
-                                       , defaultForm) where 
+                                       , defaultForm) where
 
-import           Data.Aeson                   (FromJSON, ToJSON)
+import           Data.Aeson                          (FromJSON, ToJSON)
 
-import           GHC.Generics                 (Generic)
-import           Kiosk.Backend.Form.Element.Company ( Company
-                                                    , defaultCompany)
-import           Kiosk.Backend.Form.Element.Address (Address
-                                                    , defaultAddress)
-import           Kiosk.Backend.Form.Element.Logo (Logo
-                                                    , defaultLogo)                                                    
-import           Kiosk.Backend.Form.Element.Phone (Phone
-                                                    , defaultPhone)
-import Kiosk.Backend.Form.Element.Constant (Constant
-                                           ,defaultConstant)
-import Kiosk.Backend.Form.Element.Row 
+import           GHC.Generics                        (Generic)
+import           Kiosk.Backend.Form.Element.Address  (Address, defaultAddress)
+import           Kiosk.Backend.Form.Element.Company  (Company, defaultCompany)
+import           Kiosk.Backend.Form.Element.Constant (Constant, defaultConstant)
+import           Kiosk.Backend.Form.Element.Logo     (Logo, defaultLogo)
+import           Kiosk.Backend.Form.Element.Phone    (Phone, defaultPhone)
+import           Kiosk.Backend.Form.Element.Row
 
 -- A form is a list of Rows
 data Form = Form {
     _getCompany   :: Company
   , _getAddress   :: Address
-  , _getLogo      :: Logo             
-  , _getPhone     :: Phone             
-  , _getConstants :: [Constant]              
+  , _getLogo      :: Logo
+  , _getPhone     :: Phone
+  , _getConstants :: [Constant]
   , _row          :: [Row]
  } deriving (Generic, Show)
 
@@ -48,4 +43,4 @@ instance FromJSON Form where
 
 
 defaultForm :: Form
-defaultForm = Form defaultCompany defaultAddress defaultLogo defaultPhone [defaultConstant] defaultRows       
+defaultForm = Form defaultCompany defaultAddress defaultLogo defaultPhone [defaultConstant] defaultRows
