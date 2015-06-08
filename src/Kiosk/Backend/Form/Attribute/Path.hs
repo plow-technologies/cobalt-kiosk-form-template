9fe6f929a8fe18943f90f2ef546b4c1f5db04a23
Scott Murphy <scottmurphy09@gmail.com>
2015-06-08 15:03:17 -0500

    commit before branch (like an idiot)

--- src/Kiosk/Backend/Form/Attribute/Path.hs
+++ src/Kiosk/Backend/Form/Attribute/Path.hs
@@ -1,16 +1,17 @@
-{-# LANGUAGE RankNTypes        #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes        #-}
 
 module Kiosk.Backend.Form.Attribute.Path (PathAttribute(..)) where
 
-import Kiosk.Backend.Form.Attribute
-import qualified Data.Text as T
-import Text.Read   (readMaybe)
-import           GHC.Generics           (Generic)
+import           Data.Aeson                   (FromJSON, ToJSON)
+import qualified Data.Text                    as T
+import           GHC.Generics                 (Generic)
+import           Kiosk.Backend.Form.Attribute
+import           Text.Read                    (readMaybe)
 
-data PathAttribute = PathAttribute { 
-	_getPath :: T.Text 
+data PathAttribute = PathAttribute {
+	_getPath :: T.Text
 } deriving (Generic,Show,Eq,Ord)
 
 instance AttributeClass PathAttribute where
@@ -19,3 +20,8 @@ instance AttributeClass PathAttribute where
   									(Just v') -> Right (PathAttribute v')
   									Nothing -> Left $ T.concat ["PathAttribute value not parsing -->",v]
   fromAttribute (Attribute other _) = wrongAttrResponse "path" other
+
+
+
+instance ToJSON PathAttribute where
+instance FromJSON PathAttribute where
