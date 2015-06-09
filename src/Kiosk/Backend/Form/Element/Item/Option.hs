0cf693eee28530184702fe0c082823665b36c0ed
Scott Murphy <scottmurphy09@gmail.com>
2015-06-09 00:33:41 -0500

    Factor done

--- src/Kiosk/Backend/Form/Element/Item/Option.hs
+++ src/Kiosk/Backend/Form/Element/Item/Option.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 
 {- |
@@ -17,3 +19,55 @@ Portability :  portable
 
 
 module  Kiosk.Backend.Form.Element.Item.Option  where
+import           Data.Aeson                            (FromJSON, ToJSON)
+import qualified Data.Text                             as T
+import           GHC.Generics                          (Generic)
+import           Kiosk.Backend.Form.Attribute          (Attribute (..),
+                                                        AttributeClass (..))
+import           Kiosk.Backend.Form.Element.Item.Input
+import           Kiosk.Backend.Form.Element.Item.Label (Label (..),
+                                                        defaultLabel)
+data Option = Option {
+    _getOptionText :: T.Text
+  , _optionAttrib  :: [OptionAttributes]
+} deriving (Generic, Show)
+
+
+instance ToJSON Option where
+instance FromJSON Option where
+
+data OptionAttributes = OptionNull deriving (Show,Generic)
+
+
+instance ToJSON OptionAttributes where
+instance FromJSON OptionAttributes where
+
+instance AttributeClass OptionAttributes where
+        toAttribute (OptionNull) = Attribute "" ""
+        fromAttribute _ = Right OptionNull
+
+
+data OptionQualifier = OptionQualifier {
+    _getOptionQualifierText :: [QualifierChoices]
+  , _optionQualifierAttrib  :: [OptionQualifierAttributes]
+} deriving (Generic, Show )
+
+
+instance ToJSON OptionQualifier where
+instance FromJSON OptionQualifier where
+
+data OptionQualifierAttributes = OptionQualifierNull deriving (Show,Generic)
+
+
+instance ToJSON OptionQualifierAttributes where
+instance FromJSON OptionQualifierAttributes where
+
+instance AttributeClass OptionQualifierAttributes where
+        toAttribute (OptionQualifierNull) = Attribute "" ""
+        fromAttribute _ = Right OptionQualifierNull
+
+data QualifierChoices = QualifierInput Input | QualifierLabel Label deriving (Show,Generic)
+
+
+instance ToJSON QualifierChoices where
+instance FromJSON QualifierChoices where
--- src/Kiosk/Backend/Form/Element/Item/Radio.hs
+++ src/Kiosk/Backend/Form/Element/Item/Radio.hs
@@ -22,7 +22,7 @@ import           Kiosk.Backend.Form.Element.Item.Label  (Label (..),
 import           Data.Aeson                             (FromJSON, ToJSON)
 import           Kiosk.Backend.Form.Attribute.Indexable
 import           Kiosk.Backend.Form.Attribute.Width
-
+import           Kiosk.Backend.Form.Element.Item.Option
 -- A Radio button is a circular, singular selector
 -- Our radio buttons come preloaded with Options!
 
@@ -37,52 +37,6 @@ data Radio = Radio {
 
 instance ToJSON Radio where
 instance FromJSON Radio where
-
-data Option = Option {
-    _getOptionText :: T.Text
-  , _optionAttrib  :: [OptionAttributes]
-} deriving (Generic, Show)
-
-
-instance ToJSON Option where
-instance FromJSON Option where
-
-data OptionAttributes = OptionNull deriving (Show,Generic)
-
-
-instance ToJSON OptionAttributes where
-instance FromJSON OptionAttributes where
-
-instance AttributeClass OptionAttributes where
-	toAttribute (OptionNull) = Attribute "" ""
-	fromAttribute _ = Right OptionNull
-
-
-data OptionQualifier = OptionQualifier {
-    _getOptionQualifierText :: [QualifierChoices]
-  , _optionQualifierAttrib  :: [OptionQualifierAttributes]
-} deriving (Generic, Show )
-
-
-instance ToJSON OptionQualifier where
-instance FromJSON OptionQualifier where
-
-data OptionQualifierAttributes = OptionQualifierNull deriving (Show,Generic)
-
-
-instance ToJSON OptionQualifierAttributes where
-instance FromJSON OptionQualifierAttributes where
-
-instance AttributeClass OptionQualifierAttributes where
-	toAttribute (OptionQualifierNull) = Attribute "" ""
-	fromAttribute _ = Right OptionQualifierNull
-
-data QualifierChoices = QualifierInput Input | QualifierLabel Label deriving (Show,Generic)
-
-
-instance ToJSON QualifierChoices where
-instance FromJSON QualifierChoices where
-
 defaultRadio :: Radio
 defaultRadio = Radio defaultLabel [defaultOption] [defaultOptionQualifier]
 
